67040fd17516de2309b3031d71b0486e290fcd3f
Timur Jalilov <timurjalilov1@gmail.com>
2021-09-18 21:08:28 +0100

    added lap functionality

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const setMinutes = document.getElementById("minutes");
-const setSeconds = document.getElementById("seconds");
-const setMilliseconds = document.getElementById("milliseconds");
-const setTime = document.getElementById("time");
-const startStopButton = document.getElementById("startStop");
-const resetLapButton = document.getElementById("resetLap");
+const setTime = document.getElementById("setTime");
 const lapTable = document.getElementById("lapTable");
+let setActiveLapNumber;
+let setActiveLapTime;
+
+const startStopButton = document.getElementById("startStopButton");
+const resetLapButton = document.getElementById("resetLapButton");
 
 let startTime = null;
 let requestId = null;
-let started;
-let paused;
-let ableToReset;
+let started = false;
+let ableToReset = false;
+let lapActive = false;
 
 let milliseconds = 0;
 let seconds = 0;
@@ -18,86 +18,114 @@ let minutes = 0;
 let lapNumber = 0;
 let elapsedTime = 0;
 let stopTime = 0;
-let lastLapTime = 0;
+let lapResetTime = 0;
+let lapTime = 0;
+let previousLapTime = 0;
 
-const stopwatch = (timestamp) => {
+const timer = (timestamp) => {
   if (!startTime) {
     startTime = timestamp;
   }
 
-  if (started && !paused) {
-    elapsedTime = timestamp - startTime - stopTime;
+  elapsedTime = timestamp + stopTime - startTime;
 
-    requestAnimationFrame(stopwatch);
-  } else if (started && paused) {
-    stopTime = timestamp - startTime - elapsedTime;
+  setTime.innerHTML = convertTime(elapsedTime);
 
-    elapsedTime = timestamp - startTime - stopTime;
+  setActiveLapTime.innerHTML = convertTime(elapsedTime - lapResetTime);
 
-    paused = false;
-
-    requestAnimationFrame(stopwatch);
-  }
-
-  displayTime(elapsedTime);
-};
-
-const displayTime = (time) => {
-  convertTime(time);
-  setTime.innerHTML = `${padNumbers(minutes)}:${padNumbers(
-    seconds
-  )}.${padNumbers(Math.floor(milliseconds / 10))}`;
+  requestId = requestAnimationFrame(timer);
 };
 
 const convertTime = (time) => {
   milliseconds = Math.floor(time) % 1000;
   seconds = Math.floor(time / 1000) % 60;
   minutes = Math.floor(time / 60000) % 60;
+
+  return `${padNumbers(minutes)}:${padNumbers(seconds)}.${padNumbers(
+    Math.floor(milliseconds / 10)
+  )}`;
 };
 
 const padNumbers = (number) => {
   return number.toString().padStart(2, 0);
 };
 
-const startStop = () => {
-  if (!started) {
-    requestAnimationFrame(stopwatch);
-    startStopButton.innerHTML = "Stop";
-    resetLapButton.innerHTML = "Lap";
-    ableToReset = false;
-    started = true;
-  } else {
-    startStopButton.innerHTML = "Start";
-    resetLapButton.innerHTML = "Reset";
-    ableToReset = true;
-    started = false;
-    paused = true;
-  }
+const start = () => {
+  requestAnimationFrame(timer);
+  startStopButton.innerHTML = "Stop";
+  resetLapButton.innerHTML = "Lap";
+  ableToReset = false;
+  started = true;
 };
 
-const resetLap = () => {
-  if (ableToReset) {
-    elapsedTime = null;
-    setTime.innerHTML = "00:00.00";
-    lapNumber = 0;
-    lapTable.innerHTML = "";
-  } else {
-    if (started) {
-      saveLap();
-    }
+const stop = () => {
+  cancelAnimationFrame(requestId);
+
+  ableToReset = true;
+  started = false;
+  startTime = null;
+
+  stopTime = elapsedTime;
+
+  startStopButton.innerHTML = "Start";
+  resetLapButton.innerHTML = "Reset";
+};
+
+const reset = () => {
+  elapsedTime = null;
+  lapActive = false;
+
+  lapResetTime = 0;
+  lapNumber = 0;
+  stopTime = 0;
+  previousLapTime = 0;
+
+  setTime.innerHTML = "00:00.00";
+  lapTable.innerHTML = "";
+};
+
+const createActiveLap = () => {
+  if (!lapActive) {
+    const row = lapTable.insertRow(0);
+    setActiveLapNumber = row.insertCell(0);
+    setActiveLapTime = row.insertCell(1);
+
+    setActiveLapNumber.innerHTML = `Lap 1`;
   }
+  lapActive = true;
 };
 
 const saveLap = () => {
+  const row = lapTable.insertRow(1);
+  const setLapNumber = row.insertCell(0);
+  const setLapTime = row.insertCell(1);
+
   lapNumber++;
-  let row = lapTable.insertRow(0);
-  let setLapNumber = row.insertCell(0);
-  let setLapTime = row.insertCell(1);
+  lapResetTime = elapsedTime;
+
+  lapTime = elapsedTime - previousLapTime;
+  previousLapTime = elapsedTime;
+
   setLapNumber.innerHTML = `Lap ${lapNumber}`;
-  setLapTime.innerHTML = `${padNumbers(minutes)}:${padNumbers(
-    seconds
-  )}.${padNumbers(Math.floor(milliseconds / 10))}`;
+  setLapTime.innerHTML = convertTime(lapTime);
+  setActiveLapNumber.innerHTML = `Lap ${lapNumber + 1}`;
 };
 
-startStopButton.onclick = startStop;
-resetLapButton.onclick = resetLap;
+const fastestSlowestLap = () => {};
+
+startStopButton.onclick = () => {
+  if (!started) {
+    createActiveLap();
+    start();
+  } else {
+    stop();
+  }
+};
+
+resetLapButton.onclick = () => {
+  if (ableToReset) {
+    reset();
+  } else {
+    saveLap();
+  }
+};
