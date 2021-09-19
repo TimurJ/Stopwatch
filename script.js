051e260ecbc673d2c839d3e2002af390124ddab5
Timur Jalilov <timurjalilov1@gmail.com>
2021-09-19 16:21:00 +0100

    finalised styling and functionality

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const setTime = document.getElementById("setTime");
 const lapTable = document.getElementById("lapTable");
-let setActiveLapNumber;
-let setActiveLapTime;
 
 const startStopButton = document.getElementById("startStopButton");
 const resetLapButton = document.getElementById("resetLapButton");
+
+let setActiveLapNumber;
+let setActiveLapTime;
+let setLapNumber;
+let setLapTime;
+let lapRow;
 
 let startTime = null;
 let requestId = null;
@@ -15,12 +19,15 @@ let lapActive = false;
 let milliseconds = 0;
 let seconds = 0;
 let minutes = 0;
-let lapNumber = 0;
 let elapsedTime = 0;
 let stopTime = 0;
-let lapResetTime = 0;
+
+let lapNumber = 0;
 let lapTime = 0;
+let lapResetTime = 0;
 let previousLapTime = 0;
+let fastestLap = 0;
+let slowestLap = 0;
 
 const timer = (timestamp) => {
   if (!startTime) {
@@ -52,7 +59,6 @@ const padNumbers = (number) => {
 
 const start = () => {
   requestAnimationFrame(timer);
-  startStopButton.innerHTML = "Stop";
   resetLapButton.innerHTML = "Lap";
   ableToReset = false;
   started = true;
@@ -66,9 +72,6 @@ const stop = () => {
   startTime = null;
 
   stopTime = elapsedTime;
-
-  startStopButton.innerHTML = "Start";
-  resetLapButton.innerHTML = "Reset";
 };
 
 const reset = () => {
@@ -79,9 +82,21 @@ const reset = () => {
   lapNumber = 0;
   stopTime = 0;
   previousLapTime = 0;
+  fastestLap = 0;
+  slowestLap = 0;
 
   setTime.innerHTML = "00:00.00";
+};
+
+const resetTable = () => {
   lapTable.innerHTML = "";
+  for (let i = 0; i < 6; i++) {
+    const row = lapTable.insertRow(0);
+    const setFirstCell = row.insertCell(0);
+    const setSecondCell = row.insertCell(1);
+    setFirstCell.innerHTML = "";
+    setSecondCell.innerHTML = "";
+  }
 };
 
 const createActiveLap = () => {
@@ -90,15 +105,21 @@ const createActiveLap = () => {
     setActiveLapNumber = row.insertCell(0);
     setActiveLapTime = row.insertCell(1);
 
+    lapTable.deleteRow(6);
+
     setActiveLapNumber.innerHTML = `Lap 1`;
   }
   lapActive = true;
 };
 
 const saveLap = () => {
-  const row = lapTable.insertRow(1);
-  const setLapNumber = row.insertCell(0);
-  const setLapTime = row.insertCell(1);
+  lapRow = lapTable.insertRow(1);
+  setLapNumber = lapRow.insertCell(0);
+  setLapTime = lapRow.insertCell(1);
+
+  if (lapNumber < 5) {
+    lapTable.deleteRow(lapNumber + 2);
+  }
 
   lapNumber++;
   lapResetTime = elapsedTime;
@@ -111,21 +132,90 @@ const saveLap = () => {
   setActiveLapNumber.innerHTML = `Lap ${lapNumber + 1}`;
 };
 
-const fastestSlowestLap = () => {};
+const fastestSlowestLap = () => {
+  const lapTableRowOne = lapTable.rows[1];
+  const lapTableRowTwo = lapTable.rows[2];
+
+  if (!fastestLap) {
+    fastestLap = lapTime;
+  }
+
+  if (lapNumber === 2) {
+    if (fastestLap > lapTime) {
+      slowestLap = fastestLap;
+      fastestLap = lapTime;
+      lapTableRowOne.classList.add("fastestLap");
+      lapTableRowTwo.classList.add("slowestLap");
+    } else {
+      slowestLap = lapTime;
+      lapTableRowOne.classList.add("fastestLap");
+      lapTableRowTwo.classList.add("slowestLap");
+    }
+  }
+
+  if (lapNumber > 2) {
+    if (fastestLap && lapTime < fastestLap) {
+      fastestLap = lapTime;
+      $("#lapTable tr").removeClass("fastestLap");
+      lapRow.classList.add("fastestLap");
+    }
+
+    if (slowestLap && lapTime > slowestLap) {
+      slowestLap = lapTime;
+      $("#lapTable tr").removeClass("slowestLap");
+      lapRow.classList.add("slowestLap");
+    }
+  }
+};
+
+const changeToStopButton = () => {
+  startStopButton.classList.remove("startButton");
+  startStopButton.classList.add("stopButton");
+  startStopButton.innerHTML = "Stop";
+};
+
+const changeToStartButton = () => {
+  startStopButton.classList.remove("stopButton");
+  startStopButton.classList.add("startButton");
+  startStopButton.innerHTML = "Start";
+};
+
+const activateLapButton = () => {
+  resetLapButton.classList.remove("initialLapButton");
+  resetLapButton.classList.add("resetLapButton");
+  resetLapButton.innerHTML = "Lap";
+};
+
+const deactivateLapButton = () => {
+  resetLapButton.classList.remove("resetLapButton");
+  resetLapButton.classList.add("initialLapButton");
+  resetLapButton.innerHTML = "Lap";
+};
+
+const changeToResetButton = () => {
+  resetLapButton.innerHTML = "Reset";
+};
 
 startStopButton.onclick = () => {
   if (!started) {
     createActiveLap();
     start();
+    changeToStopButton();
+    activateLapButton();
   } else {
     stop();
+    changeToStartButton();
+    changeToResetButton();
   }
 };
 
 resetLapButton.onclick = () => {
   if (ableToReset) {
     reset();
+    resetTable();
+    deactivateLapButton();
   } else {
     saveLap();
+    fastestSlowestLap();
   }
 };
